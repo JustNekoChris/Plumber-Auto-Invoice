1db4a2e2d948bf1a9d3a9d312dd80f7bb5cab96c
JustNekoChris <chbarajas20@gmail.com>
2025-07-02 19:24:08 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log('Hello World!')
+
+const env = process.env.NODE_ENV || 'development';
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+if (env === 'development') {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+        hardResetMethod: 'exit'
+    });
+}
+
+const createWindow = () => { 
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        },
+        titleBarStyle: 'hidden',
+        ...(process.platform !== 'darwin' ? { titleBarOverlay: true } : {})
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit()
+})
